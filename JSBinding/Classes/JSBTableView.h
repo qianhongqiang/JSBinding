8950d6fbc1b821921fb43047d3b4ff45abf8f27f
zexun <hongqiang.qhq@koubei.com>
2017-07-04 11:39:13 +0800

    UITableview binding

--- JSBinding/Classes/JSBTableView.h
+++ JSBinding/Classes/JSBTableView.h
@@ -17,7 +17,37 @@
 
 - (NSInteger)numberOfRowsInSection:(NSInteger)section;
 
+- (CGRect)rectForSection:(NSInteger)section;
+- (CGRect)rectForHeaderInSection:(NSInteger)section;
+- (CGRect)rectForFooterInSection:(NSInteger)section;
+- (CGRect)rectForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+- (NSIndexPath *)indexPathForRowAtPoint:(CGPoint)point;
+- (NSIndexPath *)indexPathForCell:(UITableViewCell *)cell;
+- (NSArray<NSIndexPath *> *)indexPathsForRowsInRect:(CGRect)rect;
+
+- (UITableViewCell *)cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+@property (nonatomic, readonly) NSArray *visibleCells;
+@property (nonatomic, readonly) NSArray<NSIndexPath *> *indexPathsForVisibleRows;
+
+- (UITableViewHeaderFooterView *)headerViewForSection:(NSInteger)section;
+- (UITableViewHeaderFooterView *)footerViewForSection:(NSInteger)section;
+
 - (void)scrollToRowAtIndexPath:(NSIndexPath *)indexPath atScrollPosition:(UITableViewScrollPosition)scrollPosition animated:(BOOL)animated;
+- (void)scrollToNearestSelectedRowAtScrollPosition:(UITableViewScrollPosition)scrollPosition animated:(BOOL)animated;
+
+- (void)beginUpdates;
+- (void)endUpdates;
+
+- (void)insertSections:(NSIndexSet *)sections withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)deleteSections:(NSIndexSet *)sections withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)reloadSections:(NSIndexSet *)sections withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)moveSection:(NSInteger)section toSection:(NSInteger)newSection;
+
+- (void)insertRowsAtIndexPaths:(NSArray<NSIndexPath *> *)indexPaths withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)deleteRowsAtIndexPaths:(NSArray<NSIndexPath *> *)indexPaths withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)reloadRowsAtIndexPaths:(NSArray<NSIndexPath *> *)indexPaths withRowAnimation:(UITableViewRowAnimation)animation;
+- (void)moveRowAtIndexPath:(NSIndexPath *)indexPath toIndexPath:(NSIndexPath *)newIndexPath;
 
 @end
 
