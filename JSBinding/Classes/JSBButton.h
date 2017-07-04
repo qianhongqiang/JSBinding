0c285d68e10a12a11b425ec4fd76d8ff3a46f3ee
zexun <hongqiang.qhq@koubei.com>
2017-07-04 11:21:47 +0800

    UIButton binding

--- JSBinding/Classes/JSBButton.h
+++ JSBinding/Classes/JSBButton.h
@@ -9,7 +9,18 @@
 #import <UIKit/UIKit.h>
 #import <JavaScriptCore/JavaScriptCore.h>
 
-@interface JSBButton : UIButton
+@protocol UIButtonJSBindingProtocol <NSObject,JSExport>
+
+- (void)setTitle:(NSString *)title forState:(UIControlState)state;
+- (void)setTitleColor:(UIColor *)color forState:(UIControlState)state;
+- (void)setTitleShadowColor:(UIColor *)color forState:(UIControlState)state;
+- (void)setImage:(UIImage *)image forState:(UIControlState)state;
+- (void)setBackgroundImage:(UIImage *)image forState:(UIControlState)state;
+- (void)setAttributedTitle:(NSAttributedString *)title forState:(UIControlState)state;
+
+@end
+
+@interface JSBButton : UIButton<UIButtonJSBindingProtocol>
 
 @property (nonatomic, weak) JSContext *context;
 
